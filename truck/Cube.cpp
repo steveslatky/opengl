3de1f97a2a5fc2beefdc96caf1967b339c3ca9db
Stephen Slatky <steve.slatky@gmail.com>
2018-03-20 15:55:54 -0400

    updated cube

--- truck/Cube.cpp
+++ truck/Cube.cpp
@@ -1,27 +1,59 @@
 #include "Cube.h"
 
 Cube::Cube() {
-	buildCube();
-
-	//get buffers for attributes and indices
-	glGenBuffers(1, &VBO);
-
-	//put the data on the VBO
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
-
-	//set up stuff for the body of the cube
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO); //make this VAO active
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);  //associate the VBO with the active VAO
-
-	assert((program = InitShader("vshaderCube_v150.glsl", "fshaderCube_v150.glsl"))!=-1);
-	glUseProgram(program);  //make it the active one
-
-	assert((vPosition = glGetAttribLocation(program, "vPosition")) != -1);
-	glEnableVertexAttribArray(vPosition);  //enable it
-	glVertexAttribPointer(vPosition, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
-
+    buildCube();
+    
+    //get buffers for attributes and indices
+    glGenBuffers(1, &VBO);
+    
+    //put the data on the VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexLocations) + sizeof(vertexNormals) + sizeof(vertexTextureLocations),NULL, GL_STATIC_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertexLocations), vertexLocations);
+    glBufferSubData(GL_ARRAY_BUFFER, sizeof(vertexLocations), sizeof(vertexNormals), vertexNormals);
+    glBufferSubData(GL_ARRAY_BUFFER, sizeof(vertexLocations) + sizeof(vertexNormals), sizeof(vertexTextureLocations), vertexTextureLocations);
+    
+    //set up stuff for the body of the cube
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO); //make this VAO active
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);  //associate the VBO with the active VAO
+    
+    assert((program = InitShader("vshaderCube_v150.glsl", "fshaderCube_v150.glsl"))!=-1);
+    glUseProgram(program);  //make it the active one
+    
+    assert((vPosition = glGetAttribLocation(program, "vPosition")) != -1);
+    glEnableVertexAttribArray(vPosition);  //enable it
+    glVertexAttribPointer(vPosition, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+    
+    nPosition = glGetAttribLocation(program, "vNormal");
+    glEnableVertexAttribArray(nPosition);
+    glVertexAttribPointer(nPosition, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sizeof(vertexLocations)));
+    
+    vTexture = glGetAttribLocation(program, "vTexture");
+    glEnableVertexAttribArray(vTexture);
+    glVertexAttribPointer(vTexture, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sizeof(vertexLocations) + sizeof(vertexNormals)));
+    
+    //get the texture data for the quad
+    glGenTextures(1, &texture);
+    int imgsize = 512;
+    //    int height = 837;
+    //    int width = 750;
+    //837x750
+    GLubyte *data = ppmRead("food-truck.ppm", &imgsize, &imgsize);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imgsize, imgsize, 0, GL_RGB, GL_UNSIGNED_BYTE, data);  //move the data onto the GPU
+    delete[] data;  //dont' need this data now that its on the GPU
+    
+    //set the texturing parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    
+    assert((tex_loc = glGetUniformLocation(program, "textureID")) != -1);
+    
+    //    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imgsize, imgsize, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    
     //get the locations of the shader variables
     assert((mmLoc = glGetUniformLocation(program, "model_matrix"))!=-1);
     assert((cmLoc = glGetUniformLocation(program, "cam_matrix")) != -1);
@@ -45,41 +77,92 @@ Cube::Cube() {
 }
 
 Cube::~Cube(){
-
+    
 }
 
 //----------------------------------------------------------------------------
 void Cube::makeQuad(int ind1, int ind2, int ind3, int ind4) {
-	//triangle #1
-	points[index] = vertices[ind1];
-	index++;
+    
+    //triangle #1
+    points[index] = vertices[ind1];
+    vertexTextureLocations[index] = vec2(0,0);
+    index++;
+    
+    points[index] = vertices[ind2];
+    vertexTextureLocations[index] = vec2(1,0);
+    index++;
+    
+    points[index] = vertices[ind3];
+    vertexTextureLocations[index] = vec2(1,1);
+    index++;
+    
+    //Triangle #2
+    points[index] = vertices[ind3];
+    vertexTextureLocations[index] = vec2(1,1);
+    index++;
+    
+    points[index] = vertices[ind4];
+    vertexTextureLocations[index] = vec2(0,1);
+    index++;
+    
+    points[index] = vertices[ind1];
+    vertexTextureLocations[index] = vec2(0,0);
+    index++;
+}
 
-	points[index] = vertices[ind2];
-	index++;
-
-	points[index] = vertices[ind3];
-	index++;
-
-	//Triangle #2
-	points[index] = vertices[ind3];
-	index++;
-
-	points[index] = vertices[ind4];
-	index++;
-
-	points[index] = vertices[ind1];
-	index++;
+void Cube::makeIndexedQuad(GLubyte ind1, GLubyte ind2, GLubyte ind3, GLubyte ind4) {
+    
+    //Triangle 1
+    vec3 N = normalize(cross(vertices[ind2] - vertices[ind1], vertices[ind3] - vertices[ind1]));
+    vertexLocations[index] = vertices[ind1];
+    vertexNormals[index] = N;
+    vertexTextureLocations[index] = vec2(0,0);
+    index++;
+    
+    vertexLocations[index] = vertices[ind2];
+    vertexNormals[index] = N;
+    vertexTextureLocations[index] = vec2(1,0);
+    index++;
+    
+    vertexLocations[index] = vertices[ind3];
+    vertexNormals[index] = N;
+    vertexTextureLocations[index] = vec2(1,1);
+    index++;
+    
+    //Triangle 2
+    N = normalize(cross(vertices[ind3] - vertices[ind1], vertices[ind4] - vertices[ind1]));
+    vertexLocations[index] = vertices[ind3];
+    vertexNormals[index] = N;
+    vertexTextureLocations[index] = vec2(1,1);
+    index++;
+    
+    vertexLocations[index] = vertices[ind4];
+    vertexNormals[index] = N;
+    vertexTextureLocations[index] = vec2(0,1);
+    index++;
+    
+    vertexLocations[index] = vertices[ind1];
+    vertexNormals[index] = N;
+    vertexTextureLocations[index] = vec2(0,0);
+    index++;
 }
 
 void Cube::buildCube() {
-	index = 0;
-
-	makeQuad(1, 0, 3, 2);  //front
-	makeQuad(2, 3, 7, 6);  //right
-	makeQuad(3, 0, 4, 7);  //bottom
-	makeQuad(6, 5, 1, 2);  //top
-	makeQuad(4, 5, 6, 7);  //back
-	makeQuad(5, 4, 0, 1);  //left
+    index = 0;
+    
+    //    makeQuad(1, 0, 3, 2);  //front
+    //    makeQuad(2, 3, 7, 6);  //right
+    //    makeQuad(3, 0, 4, 7);  //bottom
+    //    makeQuad(6, 5, 1, 2);  //top
+    //    makeQuad(4, 5, 6, 7);  //back
+    //    makeQuad(5, 4, 0, 1);  //left
+    
+    makeIndexedQuad(1, 0, 3, 2);  //front
+    makeIndexedQuad(2, 3, 7, 6);  //right
+    makeIndexedQuad(3, 0, 4, 7);  //bottom
+    makeIndexedQuad(6, 5, 1, 2);  //top
+    makeIndexedQuad(4, 5, 6, 7);  //back
+    makeIndexedQuad(5, 4, 0, 1);  //left
 }
 
 //set the materials
@@ -91,10 +174,10 @@ void Cube::setMaterial(vec4 d, vec4 a, vec4 s, float sh) {
 }
 
 void Cube::draw(Camera cam, vector<Light> lights){
-
-	//Draw the cube body
-	glBindVertexArray(VAO);
-	glUseProgram(program);  //also switch to using this shader program
+    
+    //Draw the cube body
+    glBindVertexArray(VAO);
+    glUseProgram(program);  //also switch to using this shader program
     
     //set all the uniform variables
     glUniformMatrix4fv(mmLoc, 1, GL_TRUE, modelmatrix);
@@ -104,6 +187,13 @@ void Cube::draw(Camera cam, vector<Light> lights){
     glUniform4fv(mdLoc, 1, matDiffuse);
     glUniform4fv(msLoc, 1, matSpecular);
     glUniform1f(mAlphaLoc, shininess);
+    glUniform1i(tex_loc, 0);
+    
+    
+    //enable the necessary texturing
+    glEnable(GL_TEXTURE_2D);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
     
     // spotlight
     glUniform1i(enabled1, lights[0].enabled);
@@ -119,11 +209,12 @@ void Cube::draw(Camera cam, vector<Light> lights){
     glUniform4fv(ldLoc2, 1, lights[1].getDiffuse());
     glUniform4fv(lsLoc2, 1, lights[1].getSpecular());
     
-	glDrawArrays(GL_TRIANGLES, 0, 6 * 2 * 3);
+    glDrawArrays(GL_TRIANGLES, 0, 6 * 2 * 3);
 }
 
 bool Cube::testCollision(vec4 rayOrigin, vec4 rayDirection)
 {
-    // We do not need this for the cube
     return false;
+    // We do not need this for the cube
 }
+
--- truck/Cube.h
+++ truck/Cube.h
@@ -11,19 +11,30 @@
 using namespace std;
 
 class Cube: public Drawable{
-
-
+    
+    
 public:
-	Cube();
-	~Cube();
-	void draw(Camera, vector<Light>);
+    Cube();
+    ~Cube();
+    void draw(Camera, vector<Light>);
     bool testCollision(vec4 rayOrigin, vec4 rayDirection);
     void setMaterial(vec4, vec4, vec4, float);
-
+    
 private:
-	void buildCube();
-	void makeQuad(int, int, int, int);
-    GLuint vPosition;
+    void buildCube();
+    //    void makeQuad(vec4, vec4, vec4, vec4);
+    void makeQuad(int, int, int, int);
+    GLuint vPosition, vTexture, nPosition;
+    
+    //uniform texture location
+    GLuint tex_loc;
+    GLuint texture;
+    
+    void makeIndexedQuad(GLubyte ind1, GLubyte ind2, GLubyte ind3, GLubyte ind4);
+    
+    vec4 vertexLocations[6 * 2 * 3];
+    vec3 vertexNormals[6 * 2 * 3];
+    vec2 vertexTextureLocations[6 * 2 * 3];
     
     //uniform transformation matrix variable locations
     GLuint mmLoc, cmLoc, pmLoc;
@@ -33,15 +44,20 @@ private:
     GLuint enabled1, laLoc1, lsLoc1, ldLoc1, lpLoc1;
     GLuint enabled2, laLoc2, lsLoc2, ldLoc2, lpLoc2;
     
+    
     //materials
     vec4 matDiffuse, matSpecular, matAmbient;
     float shininess;
-
-	unsigned int index;
-
-	vec4 vertices[8] = { vec4(-0.5,-0.5,0.5,1),vec4(-0.5,0.5,0.5,1),vec4(0.5,0.5,0.5,1),vec4(0.5,-0.5,0.5,1),
-		vec4(-0.5,-0.5,-0.5,1),vec4(-0.5,0.5,-0.5,1),vec4(0.5,0.5,-0.5,1),vec4(0.5,-0.5,-0.5,1) };
     
-	vec4 points[6 * 2 * 3]; //6 faces, 2 triangles/face, 3 vertices per triangle
+    unsigned int index;
+    
+    
+    //    vec4 vertices[8] = { vec4(-0.5,-0.5,0.5,1),vec4(-0.5,0.5,0.5,1),vec4(0.5,0.5,0.5,1),vec4(0.5,-0.5,0.5,1),
+    //        vec4(-0.5,-0.5,-0.5,1),vec4(-0.5,0.5,-0.5,1),vec4(0.5,0.5,-0.5,1),vec4(0.5,-0.5,-0.5,1) };
+    vec4 vertices[8] = { vec4(-4.75,-0.75,4.75,1),vec4(-4.75,7.75,4.75,1),vec4(4.75,7.75,4.75,1),vec4(4.75,-0.75,4.75,1),
+        vec4(-4.75,-0.75,-4.75,1),vec4(-4.75,7.75,-4.75,1),vec4(4.75,7.75,-4.75,1),vec4(4.75,-0.75,-4.75,1) };
+    
+    vec4 points[6 * 2 * 3]; //6 faces, 2 triangles/face, 3 vertices per triangle
 };
 #endif
+
